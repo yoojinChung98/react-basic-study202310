979a7215a51e1b29621ec89f9ea28f87410629ad
yoojin <yoojin9199@gmail.com>
2023-11-08 11:39:07 +0900

    Provider Consumer Auth-context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Home from './components/SideEffect/Home/Home';
 import Login from './components/SideEffect/Login/Login';
+// 컨텍스트 불러오기
+import AuthContext from './store/auth-context';
 
 const App = () => {
   // 로그인 상태를 관리하는 변수
@@ -34,16 +36,17 @@ const App = () => {
   };
 
   return (
-    <>
-      <MainHeader
-        isAuthenticated={isLoggedIn}
-        onLogout={logoutHandler}
-      />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
       </main>
-    </>
+    </AuthContext.Provider>
   );
 };
 
--- src/components/SideEffect/MainHeader/MainHeader.js
+++ src/components/SideEffect/MainHeader/MainHeader.js
@@ -3,14 +3,11 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import classes from './MainHeader.module.css';
 
-const MainHeader = ({ isAuthenticated, onLogout }) => {
+const MainHeader = ({ onLogout }) => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation
-        isLoggedIn={isAuthenticated}
-        onLogout={onLogout}
-      />
+      <Navigation onLogout={onLogout} />
     </header>
   );
 };
